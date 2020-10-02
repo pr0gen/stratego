f1737617719fd81f0f4d045a7eb9fba8bc9821fa
Rolf1e <tigran.slama@laposte.net>
2020-10-03 00:55:15 +0200

    init game-engine project

---
+++ game-engine/README.md
@@ -0,0 +1,6 @@
+I used this https://www.digitalocean.com/community/tutorials/typescript-new-project
+
+Compile : `tsc`
+Compile to dev `tsc -w`
+
+Run `mode dist/index.js`
---
+++ game-engine/src/piece.ts
@@ -0,0 +1,14 @@
+export enum Piece {
+	Bomb = -2,
+	Marshal = 10,
+	General = 9,
+	Colonel = 8,
+	Major = 7,
+	Captain = 6,
+	Lieutenant = 5,
+	Sergeant = 4,
+	Miner = 3,
+	Scout = 2,
+	Spy = 1,
+	Flag = -1
+}
