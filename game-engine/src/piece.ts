b1fca1a95b0d0e3f6347baca7d839d33d4ea8bc3
Rolf1e <tigran.slama@laposte.net>
2020-10-03 12:19:11 +0200

    piece and moves

--- game-engine/src/piece.ts
+++ game-engine/src/piece.ts
@@ -1,4 +1,76 @@
-export enum Piece {
+import { Move, AvailableMoves, create as moveCreate} from './move';
+
+export interface Piece {
+	move: Move,
+	rank: number,
+}
+
+export function create(type: PieceType) : Piece {
+	switch(type) {
+		case PieceType.Bomb:
+			return {
+			move: moveCreate(AvailableMoves.Immovable),
+			rank: PieceType.Bomb
+		};
+		case PieceType.Marshal:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Marshal
+		};
+		case PieceType.General:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.General
+		};
+		case PieceType.Colonel:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Colonel
+		};
+		case PieceType.Major:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Major
+		};
+		case PieceType.Captain:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Captain
+		};
+		case PieceType.Lieutenant:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Lieutenant
+		};
+		case PieceType.Sergeant:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Sergeant
+		};
+		case PieceType.Miner:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Miner
+		};
+		case PieceType.Scout:
+			return {
+			move: moveCreate(AvailableMoves.Scout),
+			rank: PieceType.Scout
+		};
+		case PieceType.Spy:
+			return {
+			move: moveCreate(AvailableMoves.Normal),
+			rank: PieceType.Spy
+		};
+		case PieceType.Flag:
+			return {
+			move: moveCreate(AvailableMoves.Immovable),
+			rank: PieceType.Flag
+		};
+	}
+}
+
+export enum PieceType {
 	Bomb = -2,
 	Marshal = 10,
 	General = 9,
@@ -12,3 +84,4 @@ export enum Piece {
 	Spy = 1,
 	Flag = -1
 }
+
