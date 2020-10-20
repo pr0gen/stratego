19d92bc54bece91643ba9472704df7e9ba3fb110
Rolf1e <tigran.slama@laposte.net>
2020-10-20 21:04:23 +0200

    resolve merge dependencies

--- game-engine/src/engine.ts
+++ game-engine/src/engine.ts
@@ -4,35 +4,39 @@ import { createAllPiece } from './piece/infra/piece_utils';
 import { Player } from './player/player';
 import { gameIsOver } from './engine_utils';
 
-let board = new StrategoBoard(10);
-let pieces = createAllPiece();
-let c = createCase(CaseState.Full, 0, 0, pieces[0]);
-
-board.move(c, pieces[0]);
-console.log(board.state());
-
 class GameEngine {
 
 	private board: Board;
-	private players: Player[];
+	private players: [Player, Player];
+	private turn: boolean; // Who plays
 
 	constructor(p1: Player, p2: Player) {
 		this.board = new StrategoBoard(10);
-		this.players = new Array(2);
-		this.players.push(p1);
-		this.players.push(p2);
+		this.players = [p1, p2];
+		this.turn = true;
 	}
 
 	public launch() {
 		while(!this.isOver()) {
-			//TODO
+			if(this.turn == true) {
+				this.turn = false;// next is p2
+
+
+			} else {
+				this.turn = true;// next is p1
+			}
 		}
 	}
+
+	private askMove(player: Player) {
+		//TODO use socket to ask player
+
+	}
+
 
 	private isOver(): boolean {
 		return gameIsOver();
 	}
 
 }
-
 
