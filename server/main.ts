d8598d8fa277e7ee15c1582de211ceded1b67c44
ArthurRmd <arthur.remond@protonmail.com>
2020-11-15 17:17:29 +0100

    init : Server ( Express JS + Socket IO )

---
+++ server/main.ts
@@ -0,0 +1,88 @@
+// Env
+import {config as dotenv} from "dotenv"
+dotenv()
+
+// Require
+const express = require('express')
+const socketio = require('socket.io')
+const app = express()
+const server = require('http').Server(app)
+const morgan = require('morgan')
+const io = socketio(server, { cors: { origin: '*' } })
+
+
+// Constante
+const port = process.env.PORT
+const devMode = process.env.MODE == 'DEV' //bool
+io.set('origins', '*:*');
+
+const options = {
+    root: __dirname + '/views/'
+}
+
+// Middleware
+if (devMode) {
+    app.use(morgan('dev'))
+}
+
+
+// Express
+app.get('/', (req, res) => {
+    res.sendFile('index.html', options)
+})
+
+// Socket
+
+let games = []
+
+io.on('connection', (socket) => {
+    console.log('connection : ' + socket.id)
+
+    socket.on('create-game', () => {
+        const code = generateCode()
+        console.log('create-game ' + code )
+        games[socket.id] = code
+        socket.emit('response-create-game', code)
+    })
+
+    socket.on('join-game', (code) => {
+
+        //check if code exist
+        let found = false
+        let socketOtherPlayer = null
+
+        for (let socketId in games) {
+            if (games[socketId] == code ) {
+                found = true
+                socketOtherPlayer = socketId
+            }
+        }
+        console.log(found)
+        console.log(socketOtherPlayer)
+        if (found) {
+            socket.emit('response-join-game','start game')
+            socket.to(socketOtherPlayer).emit('player-found')
+        } else {
+            socket.emit('response-join-game','code pas bon')
+        }
+
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('disconnect : ' + socket.id)
+    })
+
+
+})
+
+// Function
+
+function generateCode() {
+    return Math.round(Math.random() * 10000)
+}
+
+// Start listening
+server.listen(port, () => {
+    console.log(`Server start => http://localhost:${port}`)
+})
