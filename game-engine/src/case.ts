8b825dde85736bc1e9c4180fd125dd974f162c33
Rolfie <52386098+Rolf1e@users.noreply.github.com>
2020-11-24 22:48:41 +0100

    Game engine (#23)
    * Integrity check on the board

--- game-engine/src/case.ts
+++ game-engine/src/case.ts
@@ -1,42 +1,44 @@
 import { AvailableMoves, create as createMove } from './piece/move';
-import { Piece, PieceType } from './piece/piece';
+import { Color, Piece, PieceType } from './piece/piece';
 
 export interface Case {
-	state: CaseState,
-	x: number,
-	y: number,
-	content: Piece
+    state: CaseState,
+    x: number,
+    y: number,
+    content: Piece
 }
 
 export enum CaseState {
-	Unreachable = -1,
-	Empty = 0,
-	Full = 1
+    Unreachable = -1,
+    Empty = 0,
+    Full = 1
 }
 
 export function display(c: Case): string {
-	return "{" + c.x + "," + c.y + "}" + c.content.rank.toString();
+    const content = c.content;
+    return "S" + c.state + "{" + c.x + "," + c.y + "}"
+        + content.rank.toString() + "/C" + content.color;
 }
 
 export function createUnreachable(x: number, y: number): Case {
-	return create(
-		CaseState.Unreachable,
-		x, y,
-		{move: createMove(AvailableMoves.Immovable), rank:PieceType.Null}
-	);
+    return create(
+        CaseState.Unreachable,
+        x, y,
+        { move: createMove(AvailableMoves.Immovable), rank: PieceType.Null, color: Color.None }
+    );
 }
 
 export function createEmpty(x: number, y: number): Case {
-	return create(
-		CaseState.Empty,
-		x, y,
-		{move: createMove(AvailableMoves.Immovable), rank:PieceType.Null}
-	);
+    return create(
+        CaseState.Empty,
+        x, y,
+        { move: createMove(AvailableMoves.Immovable), rank: PieceType.Null, color: Color.None }
+    );
 }
-export function create(state: CaseState, 
-		      x: number,
-		      y: number,
-		      content: Piece): Case {
+export function create(state: CaseState,
+    x: number,
+    y: number,
+    content: Piece): Case {
 
-	return {state, x, y, content};
+    return { state, x, y, content };
 }
