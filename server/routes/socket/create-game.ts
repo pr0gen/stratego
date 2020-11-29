14b988a61a1a60bf0955a83b1f04676b534ee808
ArthurRmd <arthur.remond@protonmail.com>
2020-11-29 22:52:20 +0100

    remove : console.log createGame

--- server/routes/socket/create-game.ts
+++ server/routes/socket/create-game.ts
@@ -2,23 +2,20 @@ import generateCode from "../../utils/generateCode";
 import {Socket} from "socket.io";
 import {Room} from "../../src/Room";
 import {Rooms} from "../../src/Rooms";
+import {RoomState} from "../../src/RoomState";
 
 export default function createGame(socket: Socket, rooms : Rooms) {
 
     socket.on('create-game', () => {
         const code = generateCode()
-
         let room = new Room();
+
         room.firstPlayerId = socket.id
         room.code = code.toString()
-
-        console.log('create-game : ' + socket.id)
+        room.state = RoomState.waitingSecondPlayer
 
         rooms.addRoom(room)
-
         socket.emit('response-create-game', code)
-
-        console.log(rooms)
     })
 
 
