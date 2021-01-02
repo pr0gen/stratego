46eb8550fa2f95a395f305d1897b04badf158569
Arthur <arthur.remond@protonmail.com>
2021-01-02 16:13:50 +0100

    init : react router

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import '../styles/App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
     return (
-        <div>
-            <h1> Stratego Project</h1>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/create-game">Create Game</Link>
+                        </li>
+                        <li>
+                            <Link to="/join-game">Join Game</Link>
+                        </li>
+                        <li>
+                            <Link to="/rules">Rules</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/create-game">
+                        <CreateGame />
+                    </Route>
+                    <Route path="/join-game">
+                        <JoinGame />
+                    </Route>
+                    <Route path="/rules">
+                        <Rules />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function CreateGame() {
+    return <h2>CreateGame</h2>;
+}
+
+function JoinGame() {
+    return <h2>JoinGame</h2>;
+}
+
+function Rules() {
+    return <h2>Rules</h2>;
+}
