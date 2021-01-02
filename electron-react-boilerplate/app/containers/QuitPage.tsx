c60b29718457c5e99088384a15b61e470f528b2f
Arthur <arthur.remond@protonmail.com>
2021-01-02 14:45:58 +0100

    remove : react boilerplate 🔥

--- electron-react-boilerplate/app/containers/QuitPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Quit from '../features/quit/Quit';
-
-export default function QuitPage() {
-  return <Quit />;
-}
--- electron-react-boilerplate/app/containers/RulesPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Rules from '../features/rules/Rules';
-
-export default function RulesPage() {
-  return <Rules />;
-}
