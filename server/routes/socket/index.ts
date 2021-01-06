bdefdc564df59c53f90aa717fbad082c9e4fbf3e
Arthur <arthur.remond@protonmail.com>
2021-01-06 17:20:32 +0100

    add : socket get-all-cases

--- server/routes/socket/index.ts
+++ server/routes/socket/index.ts
@@ -4,11 +4,13 @@ import joinGame from "./join-game";
 import {Players} from "../../src/Players";
 import {Rooms} from "../../src/Rooms";
 import socketio from "socket.io";
+import getAllCases from "./get-all-cases";
 
 export default function startSockets(socket :socketio.Socket, players: Players, rooms: Rooms) {
 
     createGame(socket, rooms)
     leaveGame(socket, rooms)
     joinGame(socket, rooms)
+    getAllCases(socket)
 
 }
---
+++ server/src/GameEngine.ts
@@ -0,0 +1,42 @@
+
+
+export class GameEngine {
+
+    static initGame(){
+        return [
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ],
+            [
+                'B','Maj','G','Capt','L','Ser','Maj','G','Capt','L'
+            ]
+        ]
+    }
+
+
+
+}
