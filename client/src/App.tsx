01677bc0bd99936cc90674b0384ba8f716740a55
ArthurRmd <arthur.remond@protonmail.com>
2021-01-12 22:42:10 +0100

    add : framework css bulma + updatr ui joinGame, CreateGame, Home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,11 +10,11 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import './styles/App.css'
+import './styles/App.scss'
 
 import io from 'socket.io-client'
 import './styles/style.scss'
-import { Socket } from "./Utils/Socket";
+import {Socket} from "./Utils/Socket";
 
 const socket: SocketIOClient.Socket = io('http://localhost:3001');
 
@@ -26,36 +26,23 @@ export default function App() {
     return (
         <Router>
             <div>
-                <Route path={['/create-game' , '/join-game' , '/rules', '/game']}>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link className="nav-items" to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link className="nav-items" to="/create-game">Create Game</Link>
-                            </li>
-                            <li>
-                                <Link className="nav-items" to="/join-game">Join Game</Link>
-                            </li>
-                            <li>
-                                <Link className="nav-items" to="/rules">Rules</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                <Route path={['/create-game', '/join-game', '/rules', '/game']}>
+                    <div className="bloc-back">
+                        <Link className="nav-items button is-primary btn-back" to="/"> Back </Link>
+                    </div>
                 </Route>
 
                 <Switch>
                     <Route exact={true} path="/">
-                        <Home />
+                        <Home/>
                     </Route>
-                    <Route path="/create-game" component={CreateGame} />
+                    <Route path="/create-game" component={CreateGame}/>
                     <Route path="/join-game" component={JoinGame}/>
                     <Route path="/rules">
-                        <Rules />
+                        <Rules/>
                     </Route>
                     <Route path="/game">
-                        <Game />
+                        <Game/>
                     </Route>
                 </Switch>
             </div>
