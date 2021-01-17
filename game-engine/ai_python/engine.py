9e14207b72e75efa22030aa30aae3035813baf3c
Rolf1e <tigran.slama@laposte.net>
2021-01-17 21:42:30 +0100

    init python engine

---
+++ game-engine/ai_python/engine.py
@@ -0,0 +1,63 @@
+from typing import Tuple, List
+import enum 
+import ai_python.stratego_engine as se
+
+class Color(enum.Enum):
+    Blue = 1
+    Red = 2
+
+
+class Player:
+    name = str
+    color = Color
+
+    def __init__(self, name: str, color: Color):
+        self.name = name
+        self.color = color
+
+
+class Engine:
+    players: Tuple[type(Player), type(Player)]
+    board: se.RustStrategoBoard
+    turn: Color
+
+    def __init__(self, players: Tuple[Player, Player], board: se.RustStrategoBoard):
+        self.players = players
+        self.board = board
+        self.color = Color.Red
+
+
+class Game:
+    engine: Engine
+    uuid: int
+
+    def __init__(self, engine: Engine, uuid: int):
+        self.engine = engine
+        self.uuid = uuid
+
+
+class GamePool:
+    pool = List[Game]
+
+    def __init__(self, init_pool: List[Game]):
+        self.pool = init_pool
+
+    def register(self, game: Game):
+        self.pool.append(game)
+
+    def find_game(self, uuid: int) -> Game:
+        for game in self.pool:
+            if game.uuid == uuid:
+                return game
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ game-engine/ai_python/test/test_engine.py
@@ -0,0 +1,39 @@
+import pprint
+from typing import Tuple
+from ai_python.engine import Player, Color, Engine, GamePool, Game
+import ai_python.stratego_engine as se
+
+def test_build_player(): 
+    player = Player("Tigran", Color.Blue) 
+    assert player.name == "Tigran"
+    assert player.color == Color.Blue
+
+
+def test_build_engine():
+    engine = Engine((Player("Tigran", Color.Blue), Player("Arthur", Color.Red)), se.rust_create_empty_stratego_board())
+    player_0 = engine.players[0]
+    assert "Tigran" == player_0.name
+    assert Color.Blue == player_0.color
+
+    player_1 = engine.players[1]
+    assert "Arthur" == player_1.name
+    assert Color.Red == player_1.color
+
+    assert Color.Red == engine.color
+
+
+def test_build_pool():
+    engine = Engine((Player("Tigran", Color.Blue), Player("Arthur", Color.Red)), se.rust_create_empty_stratego_board())
+    game_pool = GamePool([])   
+    game_pool.register(Game(engine, 0))
+    assert len(game_pool.pool) == 1
+
+
+def test_find_game_by_uuid():
+    engine = Engine((Player("Tigran", Color.Blue), Player("Arthur", Color.Red)), se.rust_create_empty_stratego_board())
+    game_pool = GamePool([Game(engine, 0)])   
+
+    game = game_pool.find_game(0)
+    
+    assert game.uuid == 0
+
