d72cb38535431670e3844c2f9c8b6640c7f4f4ac
qroyer <qroyer@adexos.fr>
2021-02-03 18:11:20 +0100

    end of monte carlo

--- ai_python/src/monte_carlo.py
+++ ai_python/src/monte_carlo.py
@@ -16,15 +16,16 @@ class MonteCarloAI(StrategoAI):
         moves = board.get_available_moves_by_color(self.color)
 
         movesFormated = parse_moves(moves)
-        print(board.bgame-engine/asic_evaluation())
+
+        print(movesFormated)
         index = random.randint(0, len(movesFormated) - 1)
 
         isThereAWinningMove = False
 
         for move in movesFormated:
-
             copied_board = board.clone_board()
-            isThereAWinningMove = simulateGame(move, copied_board)
+            parsed_move = parse_move(move)
+            isThereAWinningMove = simulateGame(parsed_move, copied_board)
             if(isThereAWinningMove):
                 winning_move = move
                 break
@@ -38,6 +39,15 @@ class MonteCarloAI(StrategoAI):
             move.show()
             return move_ready(move)
 
-    def simulateGame(move, board) -> bool :
-        //TODO
-        return true
+    def simulateGame(move, copied_board) -> bool :
+        copied_board.moving((move.from_x, move.from_y), (move.to_x, move.to_y)
+        while board.rust_basic_evaluation != null :
+            moves = copied_board.get_available_moves_by_color(self.color)
+            movesFormated = parse_moves(moves)
+            index = random.randint(0, len(movesFormated) - 1)
+            current_move = moving.movesFormated[index]
+            copied_board((current_move.from_x, current_move.from_y) , (current_move.to_x , current_move.to_y)
+
+        if (copied_board.rust_basic_evaluation == self.color):
+            return true
+        return false
