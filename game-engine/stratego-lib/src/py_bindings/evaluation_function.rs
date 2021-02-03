785c607637104db7772bb87c26b68d88759b6efc
Rolf1e <tigran.slama@laposte.net>
2021-02-03 18:37:31 +0100

    uts

--- game-engine/stratego-lib/src/py_bindings/evaluation_function.rs
+++ game-engine/stratego-lib/src/py_bindings/evaluation_function.rs
@@ -2,6 +2,12 @@ use crate::board::piece::Color;
 use crate::board::Board;
 use crate::engine_utils;
 
+/// Return winner of the game, if one there is
 pub fn basic_evaluation(board: &impl Board) -> Option<Color> {
     engine_utils::game_is_over(board.state())
+}
+
+/// Evaluate material of both Players, (red, blue)
+pub fn material_evaluation(_board: &impl Board) -> ((Color, i32), (Color, i32)) {
+    todo!()
 }
--- game-engine/stratego-lib/tests/evaluation_function_test.rs
+++ game-engine/stratego-lib/tests/evaluation_function_test.rs
@@ -3,7 +3,6 @@ mod evaluation_tests {
 
     use stratego_lib::board::case::{self, Coordinate};
     use stratego_lib::board::classic_board;
-    use stratego_lib::board::classic_board::StrategoBoard;
     use stratego_lib::board::piece::Color;
     use stratego_lib::board::piece::{Piece, PieceType};
     use stratego_lib::board::Board;
@@ -12,46 +11,42 @@ mod evaluation_tests {
     #[test]
     fn should_get_winner() {
         //Win because blue can't move
-        let board = classic_board::create_empty_stratego_board();
-        let mut cases = board.state().to_owned();
+        let mut board = classic_board::create_empty_stratego_board();
         //Reds
-        cases[0][0] = case::create_full_case(
+        board.place(case::create_full_case(
             Coordinate::new(0, 0),
             Piece::new(PieceType::Flag, Color::Red),
-        );
-        cases[0][1] = case::create_full_case(
+        )).unwrap();
+        board.place(case::create_full_case(
             Coordinate::new(0, 1),
             Piece::new(PieceType::Captain, Color::Red),
-        );
+        )).unwrap();
         //Blues
-        cases[9][0] = case::create_full_case(
+        board.place(case::create_full_case(
             Coordinate::new(9, 0),
             Piece::new(PieceType::Flag, Color::Blue),
-        );
-        let board = StrategoBoard::new(cases);
+        )).unwrap();
         assert_eq!(
             Some(Color::Red),
             evaluation_function::basic_evaluation(&board)
         );
 
         //Win because red has no Flag anymore
-        let board = classic_board::create_empty_stratego_board();
-        let mut cases = board.state().to_owned();
+        let mut board = classic_board::create_empty_stratego_board();
         //Reds
-        cases[0][1] = case::create_full_case(
+        board.place(case::create_full_case(
             Coordinate::new(0, 1),
             Piece::new(PieceType::Captain, Color::Red),
-        );
+        )).unwrap();
         //Blues
-        cases[9][0] = case::create_full_case(
+        board.place(case::create_full_case(
             Coordinate::new(9, 0),
             Piece::new(PieceType::Flag, Color::Blue),
-        );
-        cases[9][1] = case::create_full_case(
+        )).unwrap();
+        board.place(case::create_full_case(
             Coordinate::new(9, 1),
             Piece::new(PieceType::Captain, Color::Blue),
-        );
-        let board = StrategoBoard::new(cases);
+        )).unwrap();
         assert_eq!(
             Some(Color::Blue),
             evaluation_function::basic_evaluation(&board)
@@ -60,30 +55,51 @@ mod evaluation_tests {
 
     #[test]
     fn should_have_no_winner_yet() {
-        let board = classic_board::create_empty_stratego_board();
-        let mut cases = board.state().to_owned();
+        let mut board = classic_board::create_empty_stratego_board();
         //Reds
-        cases[0][0] = case::create_full_case(
-            Coordinate::new(0, 0),
-            Piece::new(PieceType::Flag, Color::Red),
-        );
-        cases[0][1] = case::create_full_case(
-            Coordinate::new(0, 1),
-            Piece::new(PieceType::Captain, Color::Red),
-        );
+        board
+            .place(case::create_full_case(
+                Coordinate::new(0, 0),
+                Piece::new(PieceType::Flag, Color::Red),
+            ))
+            .unwrap();
+        board
+            .place(case::create_full_case(
+                Coordinate::new(0, 1),
+                Piece::new(PieceType::Captain, Color::Red),
+            ))
+            .unwrap();
+
         //Blues
-        cases[9][0] = case::create_full_case(
-            Coordinate::new(9, 0),
-            Piece::new(PieceType::Flag, Color::Blue),
-        );
-        cases[9][1] = case::create_full_case(
-            Coordinate::new(9, 1),
-            Piece::new(PieceType::Captain, Color::Blue),
-        );
-        let board = StrategoBoard::new(cases);
+        board
+            .place(case::create_full_case(
+                Coordinate::new(9, 0),
+                Piece::new(PieceType::Flag, Color::Blue),
+            ))
+            .unwrap();
+        board
+            .place(case::create_full_case(
+                Coordinate::new(9, 1),
+                Piece::new(PieceType::Captain, Color::Blue),
+            ))
+            .unwrap();
         assert_eq!(None, evaluation_function::basic_evaluation(&board));
 
         let board = classic_board::create_stratego_board();
         assert_eq!(None, evaluation_function::basic_evaluation(&board));
+    }
+
+    #[test]
+    fn should_get_advantage_for_reds() {
+        let mut board = classic_board::create_empty_stratego_board();
+        board.place(case::create_full_case(
+            Coordinate::new(0, 0),
+            Piece::new(PieceType::Captain, Color::Blue)
+        )).unwrap();
+
+        eprintln!("{}", board.display());
+        let (red, blue) = evaluation_function::material_evaluation(&board);
+        assert_eq!((Color::Red, 14), red);
+        assert_eq!((Color::Blue, 14), blue);
     }
 }
