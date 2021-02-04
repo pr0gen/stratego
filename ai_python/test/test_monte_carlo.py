1b5c3de6600ca798e560d0267db4815e576b3d5f
Rolf1e <tigran.slama@laposte.net>
2021-02-04 19:11:29 +0100

    fix test python montecarlo

--- ai_python/test/test_monte_carlo.py
+++ ai_python/test/test_monte_carlo.py
@@ -1,5 +1,9 @@
 from ai_python.src.utils import StrategoAI, Move, MoveBuilder, parse_moves, move_ready
+from ai_python.src.monte_carlo import MonteCarloAI
+import ai_python.src.stratego_engine as se
 
-
-
+def test_monte_carlo(): 
+    ai = MonteCarloAI('Red') 
+    move = ai.ask_next_move(se.rust_create_stratego_board())
+    
 
