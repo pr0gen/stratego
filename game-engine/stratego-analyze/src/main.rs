38896ef404fee80ba95fc05d3308971d2b5dd1af
Rolf1e <tigran.slama@laposte.net>
2021-02-09 01:13:56 +0100

    game on multi thread

--- game-engine/stratego-analyze/src/main.rs
+++ game-engine/stratego-analyze/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+
+pub mod threads;
+pub mod writter;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let file_name = args[1].clone();
+    let number_of_thread = args[2].parse::<usize>()
+                .unwrap_or_else(|e| panic!("Failed to parse number of thread, {}", e));
+    let number_of_parties = args[3].parse::<usize>()
+                .unwrap_or_else(|e| panic!("Failed to parse number of parties, {}", e));
+
+    let mut threads = Vec::new();
+
+    let per_threads = number_of_parties / number_of_thread;
+
+    for _ in 0..per_threads {
+        for j in 0..number_of_thread {
+            threads.push(threads::spawn_thread_for_stratego(j, file_name.clone()));
+        }
+    }
+
+    // Wait for all threads to be finished
+    threads.into_iter()
+        .for_each(|thread| thread.unwrap().join().unwrap());
 }
---
+++ game-engine/stratego-analyze/src/threads.rs
@@ -0,0 +1,46 @@
+use crate::writter;
+use std::thread::{self, JoinHandle};
+use stratego_lib::board::classic_board::create_stratego_board;
+use stratego_lib::board::piece::Color;
+use stratego_lib::board::Board;
+use stratego_lib::engine::Engine;
+use stratego_lib::engine::StrategoEngine;
+use stratego_lib::engine_utils::game_is_over;
+use stratego_lib::error::StrategoError;
+use stratego_lib::player::ai_player::AIPlayer;
+
+pub fn spawn_thread_for_stratego(
+    thread_number: usize,
+    file_name: String,
+) -> Result<JoinHandle<()>, StrategoError> {
+    Ok(thread::spawn(move || {
+        let mut engine = StrategoEngine::new(
+            create_stratego_board(),
+            (
+                Box::new(AIPlayer::new(Color::Red, String::from("monte_carlo"))),
+                Box::new(AIPlayer::new(Color::Blue, String::from("random"))),
+            ),
+        );
+
+        println!("{}", engine.display_by_color(&engine.get_turn()));
+        loop {
+            let board = engine.status();
+            match game_is_over(board.state()) {
+                Some(Color::Red) => {
+                    println!("Red wins");
+                    writter::write_into_file(file_name.as_str(), "Red");
+                    break;
+                }
+                Some(Color::Blue) => {
+                    println!("Blue wins");
+                    writter::write_into_file(file_name.as_str(), "Blue");
+                    break;
+                }
+                _ => match engine.moving() {
+                    Ok(_) => println!("[Thread-{}]", thread_number),
+                    Err(e) => panic!("{:#?}", e),
+                },
+            }
+        }
+    }))
+}
