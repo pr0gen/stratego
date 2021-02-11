fe50412b3551b205f2c8fcfb37395deee5b6d924
Arthur <arthur.remond@protonmail.com>
2021-02-11 21:02:00 +0100

    update : Socket

--- server/routes/socket/move-piece.ts
+++ server/routes/socket/move-piece.ts
@@ -1,4 +1,5 @@
 import socketio from "socket.io";
+import {PlayerState} from "../../src/PlayerState";
 import {Rooms} from "../../src/Rooms";
 
 
@@ -8,13 +9,20 @@ export default function movePiece(socket: socketio.Socket, rooms: Rooms) {
 
         console.log("========== Move piece ================")
         const room = rooms.getRoomByPlayerId(socket.id)
-        room?.switchTurn()
+        if (room === undefined) return
+
+        const otherPlayer = room.getOtherPlayer(socket.id)
+        const moves = await room?.setMove(socket.id, from.x, from.y_letter, to.x, to.y_letter)
 
         console.log(from)
         console.log(to)
 
+        if (otherPlayer.state == PlayerState.IsAI) {
+            const moves = await room?.aiPlay()
+        } else {
+            room?.switchTurn()
+        }
 
-        const moves = await room?.setMove(socket.id, from.x, from.y_letter, to.x, to.y_letter )
         console.log("====================================")
         socket.emit('response-move-piece', moves)
     })
--- server/routes/socket/play-with-ai.ts
+++ server/routes/socket/play-with-ai.ts
@@ -9,12 +9,14 @@ export default function playWithAi(socket: socketio.Socket, rooms: Rooms) {
 
     socket.on('play-with-ai', (aiName) => {
 
-        console.log(aiName)
+        console.log("start game with : " + aiName)
 
         let room = new Room();
         room.firstPlayer.id = socket.id
         room.secondPlayer.state = PlayerState.IsAI
+        room.secondPlayer.aiName = aiName
         room.createBoard()
+        rooms.addRoom(room)
 
         socket.emit('response-play-with-ai',{
             aiName: aiName
