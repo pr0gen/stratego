386ece4c1d5a77da6733f21b263610704c1e042b
Arthur <arthur.remond@protonmail.com>
2021-02-11 21:01:49 +0100

    update : client

--- client/src/Pages/SelectAI.tsx
+++ client/src/Pages/SelectAI.tsx
@@ -1,29 +1,51 @@
-import React, {Component} from "react";
+import React, {useEffect, useState } from "react";
+import {Redirect} from "react-router-dom"
+import {Socket} from "../Utils/Socket";
 
-class SelectAI extends Component {
+function SelectAI() {
 
-    render() {
-        return (
-            <div className="home">
+    const socket = Socket.getSocket()
+    const [canRedirect, setRedirect] = useState(false);
 
-                <h1 className="medium-title">Select AI</h1>
-                <div className="has-text-centered">
-                    <ul className="home-nav">
-                        <li>
-                            <button className="button is-primary is-large mb-5 home-link" >
-                                Random
-                            </button>
-                        </li>
-                        <li>
-                            <button className="button is-primary is-large mb-5 home-link" >
-                                Monte Carlo
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        )
+    const startGame = (AiName: string) => {
+        socket.emit('play-with-ai', AiName)
     }
+
+    useEffect(() => {
+
+        socket.on('response-play-with-ai', (iaName:string) => {
+            console.log(iaName)
+            setRedirect(true)
+        })
+
+        return () => socket.off('response-play-with-ai');
+
+    }, []);
+
+
+    return (
+        <div className="select-ai">
+            {canRedirect ? <Redirect to={"/game"} /> : null}
+
+            <h1 className="medium-title">Select AI</h1>
+            <div className="has-text-centered">
+                <ul className="home-nav">
+                    <li>
+                        <button className="button is-primary is-large mb-5 home-link"
+                                onClick={() => startGame("random")}>
+                            Random
+                        </button>
+                    </li>
+                    <li>
+                        <button className="button is-primary is-large mb-5 home-link"
+                                onClick={() => startGame("monte-carlo")}>
+                            Monte Carlo
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 
