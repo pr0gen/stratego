3b6579ba912a2b1dc86d01bceb5f31107a433f80
qroyer <qroyer@adexos.fr>
2021-02-13 22:43:38 +0100

    pandas working on test file

---
+++ ai_python/src/plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def run_game_series() :
+    csv = pd.read_csv("test_plot.csv")
+
+    return csv
---
+++ ai_python/test/test_plot.py
@@ -0,0 +1,7 @@
+import ai_python.src.plot as plot
+
+def test_reading_file():
+    test = plot.run_game_series()
+
+
+
