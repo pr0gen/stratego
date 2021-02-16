2f05e23210691a8dae18a869f4ebf0473c07eb31
qroyer <qroyer@adexos.fr>
2021-02-16 09:15:20 +0100

    change analyses in root

--- analyses.py
+++ analyses.py
@@ -1,26 +1,7 @@
+from ai_python.src.analyses import run_game_series,plot_victories
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-
-
-def run_game_series(filename) :
-    csv = pd.read_csv(filename)
-    list = []
-    for row in csv.iterrows():
-        color = row[1][0]
-        list.append(color)
-    return list;
-
-def plot_victories(filename) :
-    wins = run_game_series(filename)
-    fig = plt.figure()
-    x = ['Red','Blue']
-    y = [wins.count('Blue'),wins.count('Red')]
-    width = 0.5
-
-    plt.bar(x,y,width,color='b')
-    plt.savefig('SimpleBar.png')
-    plt.show()
 
 plot_victories(sys.argv[1])
