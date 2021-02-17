53ede20b10a80c4075a0c1347f932e8eaba30f00
qroyer <55281858+qroyer@users.noreply.github.com>
2021-02-17 13:39:18 +0100

    Feature/analyses (#97)
    * script file starting
    
    * pandas working on test file
    
    * wip plot file
    
    * plot of victories
    
    * plot end of progress,just need to fix location of analyses.py into python files
    
    * update requirements.txt
    
    * change analyses in root
    
    * remove plot call in src files
    
    * add dynamic name of graph file
    
    * update requirements.txt
    
    * ai_python/src/analyses.py
    
    * doesnot work
    
    * running test plot file
    
    * add test csv file
    
    Co-authored-by: Rolf1e <tigran.slama@laposte.net>

---
+++ ai_python/src/analyses.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def run_game_series(filename) :
+    csv = pd.read_csv(filename)
+    list = []
+    for row in csv.iterrows():
+        color = row[1][0]
+        list.append(color)
+    return list;
+
+
+def plot_victories(filename) :
+    wins = run_game_series(filename)
+    fig = plt.figure()
+    x = ['Red','Blue']
+    y = [wins.count('Blue'),wins.count('Red')]
+    width = 0.5
+
+    plt.bar(x,y,width,color='b')
+
+    filenamepng = str(filename)[:-3] + "png"
+
+    plt.savefig(filenamepng)
+    plt.show()
---
+++ ai_python/test/test_plot.py
@@ -0,0 +1,8 @@
+from ai_python.src.analyses import run_game_series
+
+def test_reading_file():
+    run_game_series('test_plot.csv')
+
+
+
+
