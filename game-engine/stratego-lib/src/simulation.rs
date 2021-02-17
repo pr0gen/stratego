79cac837f3e799de84ccabe28db63b561ad06d95
Rolf1e <tigran.slama@laposte.net>
2021-02-17 15:54:12 +0100

    prepare ground

--- game-engine/stratego-lib/src/simulation.rs
+++ game-engine/stratego-lib/src/simulation.rs
@@ -1,30 +1,39 @@
 use crate::board::board_utils;
 use crate::board::case::Coordinate;
+use crate::board::classic_board::StrategoBoard;
 use crate::board::piece::Color;
 use crate::board::Board;
-use crate::board::classic_board::StrategoBoard;
 use crate::error::StrategoError;
+use crate::py_bindings::board_wrapper::StrategoBoardWrapper;
 use rand::prelude::*;
+use std::thread::{self, JoinHandle};
 
-pub fn simulate<Stop>(
-    board: &impl Board,
-    ai_red: &dyn Fn(&StrategoBoard, &Color) -> (Coordinate, Coordinate),
-    ai_blue: &dyn Fn(&StrategoBoard, &Color) -> (Coordinate, Coordinate),
-    evaluation_function: &dyn Fn(&StrategoBoard) -> Stop,
-    iteration_max: i32,
-    stopping_critera: Stop,
-    color: Color,
-) -> Result<(Coordinate, Coordinate), StrategoError> {
+pub type Move = (Coordinate, Coordinate);
+
+pub fn simulate<Stop: Send + std::cmp::PartialEq + Sync + Clone + std::fmt::Debug>(
+    board: StrategoBoardWrapper,
+    first_ai: &'static (dyn Fn(&StrategoBoard, &Color) -> Option<Move> + Sync),
+    second_ai: &'static (dyn Fn(&StrategoBoard, &Color) -> Option<Move> + Sync),
+    evaluation_function: &'static (dyn Fn(&StrategoBoard) -> Stop + Sync),
+    iteration_max: &i32,
+    stopping_critera: &'static Stop,
+    number_of_threads: i32,
+    number_of_parties: i32,
+) -> Result<Move, StrategoError> {
     todo!();
 }
 
-pub fn choose_randomly(board: &impl Board, color: &Color) -> (Coordinate, Coordinate) {
+pub fn choose_randomly(board: &impl Board, color: &Color) -> Option<Move> {
     let moves: Vec<_> = board_utils::get_availables_moves_by_color(board, &color);
-    let mut rng = rand::thread_rng();
-    let index: usize = rng.gen_range(0, moves.len());
-    if let Some((from, to, _, _)) = moves.get(index) {
-        (*from, *to)
+    if moves.is_empty() {
+        None
     } else {
-        panic!("Something went wrong when choosing ramdomly a move")
+        let mut rng = rand::thread_rng();
+        let index: usize = rng.gen_range(0, moves.len());
+        if let Some((from, to, _, _)) = moves.get(index) {
+            Some((*from, *to))
+        } else {
+            None
+        }
     }
 }
