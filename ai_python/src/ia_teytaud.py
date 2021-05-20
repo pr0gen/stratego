159ef3c01c2d12d1dc2802b15ef636484bba6c0e
qroyer <qroyer@adexos.fr>
2021-05-20 23:09:54 +0200

    wip ia_teytaud

--- ai_python/src/ia_teytaud.py
+++ ai_python/src/ia_teytaud.py
@@ -9,14 +9,13 @@ from operator import itemgetter
 
 class ia_teytaud(StrategoAI):
     name = "ia_teytaud"
-    color = str
-
-    # cache: Cache
+    color: str
+    cache: Cache
     # piecesManager: PiecesManager
 
-    def __init__(self, color: str):
+    def __init__(self, color: str, width, height):
         self.color = color
-        self.cache = Cache(color)
+        self.cache = Cache.instance(color, width, height)
         self.piecesManager = PiecesManager()
 
     def ask_next_move(self, board: StrategoBoardWrapper) -> Tuple[Tuple[int, str], Tuple[int, str]]:
@@ -24,10 +23,19 @@ class ia_teytaud(StrategoAI):
         movesFormated = parse_moves(moves)
 
         last_move = board.get_last_coup()
-        if last_move is not None:
-            ##Pas d'information sur la piece mangée ?
-            first_piece = last_move[0]
-            second_piece = last_move[1]
+
+        #if coup is not None:
+        #    cases, won = coup
+        #    _from, to = cases
+        #    co_from = _from['coordinate']
+        #    co_to = to['coordinate']
+        #    if won:
+        #        rank = self.__convert_rank(to['content']['rank'])
+        #        self.cache.update_piece(co_from['x'], co_from['y'], co_to['x'], co_to['y'], rank)
+        #        self.cache.show()
+        #        print(board.display())
+        #    else:
+        #        self.cache.update_piece(co_from['x'], co_from['y'], co_to['x'], co_to['y'])
 
 
         ennemy_pieces = self.piecesManager.get_pieces_left()
@@ -45,15 +53,37 @@ class ia_teytaud(StrategoAI):
 
         max_pos = [x for x, y in enumerate(probValueList) if y[1] == max_prob]
 
-        ##valeur du pion ennemi le plus probable de rencontrer :     probValueList[max_pos][0]
-        ##Bouger un pion plus fort ? ou alors bouger sans prendre de pions ?
+        #valeur du pion ennemi le plus probable de rencontrer :     probValueList[max_pos][0]
+        #Bouger un pion plus fort ? ou alors bouger sans prendre de pions ?
+
+        for move in movesFormated:
+            print(move)
+            #TODO prendre une piece qui peut battre probValueList[max_pos][0] et la jouer
+
+        #TODO à changer
+        best_move = choose_randomly(board, self.color)
 
         if last_move is None :
             best_move = choose_randomly(board, self.color)
 
-        ##Remettre a jour le pieceManager ?
+        best_move_ready = move_ready(best_move)
 
-        m = move_ready(best_move)
-        print('Move:', m)
-        return m
+        #from_case = board.at(best_move_ready[0])
+        #to_case = board.at(best_move_ready[1])
+        #from_rank = self.__convert_rank(from_case['content']['rank'])
+        #to_rank = self.__convert_rank(to_case['content']['rank'])
+        #co_to = to_case['coordinate']
+        #co_from = from_case['coordinate']
+        #if to_rank is not None:
+        #    print(self.cache.show())
+        #    if from_rank < to_rank:  # we attempt to attack, but we loosed
+        #        self.cache.update_piece(co_to['x'], co_to['y'], co_to['x'], co_to['y'],
+        #                                self.__convert_rank(to_rank)
+        #                                )
+        #    else:
+        #        self.cache.delete_piece(co_to['x'], co_to['y'])
+        #    print(self.cache.show())
+
+        print('Move:', best_move_ready)
+        return best_move_ready
 
--- ai_python/test/test_monte_carlo.py
+++ ai_python/test/test_monte_carlo.py
@@ -1,7 +1,7 @@
 from ai_python.src.utils import StrategoAI, Move, MoveBuilder, parse_moves, move_ready
 from ai_python.src.monte_carlo import MonteCarloAI
 from ai_python.src.random import RandomAI
-from ai_python.src.monte_carloV2 import MonteCarloV2AI
+from ai_python.src.ia_teytaud import ia_teytaud
 from ai_python.src.stratego_engine import StrategoBoardWrapper
 
 def _play_one_turn(board, move):
@@ -33,19 +33,20 @@ def test_monte_carlo():
     print(board.display())
     ai = MonteCarloAI('Blue', 4, 2) 
     move = ai.ask_next_move(board)
-    
-def test_monte_carlo_2():
+
+
+def test_ia_teytaud():
     # cases = [
-        # ["-2B", "07B", "10B", "03B", "09B", "01B", "03B", "04B", "02B", "04B"],
-        # ["06B", "02B", "03B", "-2B", "-1B", "-2B", "02B", "02B", "02B", "03B"],
-        # ["06B", "07B", "04B", "08B", "-2B", "06B", "02B", "02B", "04B", "08B"],
-        # ["05B", "-2B", "05B", "05B", "-2B", "02B", "05B", "03B", "06B", "07B"],
-        # ["NOP", "NOP", "XXX", "XXX", "NOP", "NOP", "XXX", "XXX", "NOP", "NOP"],
-        # ["NOP", "NOP", "XXX", "XXX", "NOP", "NOP", "XXX", "XXX", "NOP", "NOP"],
-        # ["06R", "02R", "03R", "-2R", "-1R", "-2R", "02R", "02R", "02R", "03R"],
-        # ["06R", "07R", "04R", "08R", "-2R", "06R", "02R", "02R", "04R", "08R"],
-        # ["05R", "-2R", "05R", "05R", "-2R", "02R", "05R", "03R", "06R", "07R"],
-        # ["-2R", "07R", "10R", "03R", "09R", "01R", "03R", "04R", "02R", "04R"]
+    # ["-2B", "07B", "10B", "03B", "09B", "01B", "03B", "04B", "02B", "04B"],
+    # ["06B", "02B", "03B", "-2B", "-1B", "-2B", "02B", "02B", "02B", "03B"],
+    # ["06B", "07B", "04B", "08B", "-2B", "06B", "02B", "02B", "04B", "08B"],
+    # ["05B", "-2B", "05B", "05B", "-2B", "02B", "05B", "03B", "06B", "07B"],
+    # ["NOP", "NOP", "XXX", "XXX", "NOP", "NOP", "XXX", "XXX", "NOP", "NOP"],
+    # ["NOP", "NOP", "XXX", "XXX", "NOP", "NOP", "XXX", "XXX", "NOP", "NOP"],
+    # ["06R", "02R", "03R", "-2R", "-1R", "-2R", "02R", "02R", "02R", "03R"],
+    # ["06R", "07R", "04R", "08R", "-2R", "06R", "02R", "02R", "04R", "08R"],
+    # ["05R", "-2R", "05R", "05R", "-2R", "02R", "05R", "03R", "06R", "07R"],
+    # ["-2R", "07R", "10R", "03R", "09R", "01R", "03R", "04R", "02R", "04R"]
     # ]
 
     cases = [
@@ -61,8 +62,8 @@ def test_monte_carlo_2():
         ["02R", "07R", "10R", "03R", "09R", "01R", "03R", "04R", "02R", "04R"]
     ]
     board = StrategoBoardWrapper(cases)
-    ai = MonteCarloV2AI('Red', 4, 10)
-    ai_2 = RandomAI('Blue')
+    ai = ia_teytaud('Red', 4, 10)
+    ai_2 = ia_teytaud('Blue', 4, 10)
 
     # print(board.display())
     _play_many_turns(board, 500, ai, ai_2)
@@ -70,4 +71,3 @@ def test_monte_carlo_2():
     # print(board.display())
 
     assert 1 == 0
-    
